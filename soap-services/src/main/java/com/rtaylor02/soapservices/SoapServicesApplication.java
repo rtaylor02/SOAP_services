5a31232112db4cf4eb9314ed1b42c97998a123ea
rtaylor02 <taylor.rod.j@gmail.com>
2023-02-03 05:29:48 +0000

    Initial commit
    Added starting project of employee management SOAP services

---
+++ soap-services/src/main/java/com/rtaylor02/soapservices/SoapServicesApplication.java
@@ -0,0 +1,13 @@
+package com.rtaylor02.soapservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoapServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoapServicesApplication.class, args);
+	}
+
+}
---
+++ soap-services/src/test/java/com/rtaylor02/soapservices/SoapServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rtaylor02.soapservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
